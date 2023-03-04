c40806fa45d2003cc38201944f0c288a09871a98
Luis Felipe <luis.felipe32971@gmail.com>
2023-03-04 15:06:03 -0300

    update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,15 +4,15 @@ resource "aws_s3_bucket" "source-bucket" {
 }
 
 resource "aws_s3_bucket" "type-of-license-bucket" {
-  bucket = "903442739132-type-of-license-bucket/year=2021/"
+  bucket = "903442739132-type-of-license-bucket/"
 
 }
 
 resource "aws_s3_bucket" "trips-not-shared-bucket" {
-  bucket = "903442739132-trips-not-shared-bucket/year=2021/"
+  bucket = "903442739132-trips-not-shared-bucket/"
 }
 
 resource "aws_s3_bucket" "trips-more-than-10-dollars-bucket" {
-  bucket = "903442739132-trips-more-than-10-dollars-bucket/year=2021/"
+  bucket = "903442739132-trips-more-than-10-dollars-bucket/"
 }
 
