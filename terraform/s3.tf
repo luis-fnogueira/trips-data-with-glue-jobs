dd49fd1d11ab1217a23b38b616447c8d51f70381
Luis Felipe <luis.felipe32971@gmail.com>
2023-03-04 15:02:52 -0300

    completed glue notebook

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,18 +1,18 @@
 resource "aws_s3_bucket" "source-bucket" {
-  bucket = "903442739132-source-bucket-trips-data"
+  bucket = "903442739132-source-bucket-trips-data/year=2021/"
 
 }
 
 resource "aws_s3_bucket" "type-of-license-bucket" {
-  bucket = "903442739132-type-of-license-bucket"
+  bucket = "903442739132-type-of-license-bucket/year=2021/"
 
 }
 
 resource "aws_s3_bucket" "trips-not-shared-bucket" {
-  bucket = "903442739132-trips-not-shared-bucket"
+  bucket = "903442739132-trips-not-shared-bucket/year=2021/"
 }
 
 resource "aws_s3_bucket" "trips-more-than-10-dollars-bucket" {
-  bucket = "903442739132-trips-more-than-10-dollars-bucket"
+  bucket = "903442739132-trips-more-than-10-dollars-bucket/year=2021/"
 }
 
